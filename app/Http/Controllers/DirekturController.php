c719129a3f2cf8d3a4bcd4ce74427ada9fec491c
Syukran <msyukran444@gmail.com>
2023-12-11 22:00:46 +0700

    Update direktur dan model tbl_divisi

--- app/Http/Controllers/DirekturController.php
+++ app/Http/Controllers/DirekturController.php
@@ -24,12 +24,12 @@ class DirekturController extends Controller
         /* ------------------------------------- PEMASUKAN MINGGUAN------------------------------------- */
         //pemasukan 1 minggu terakhir
         $mingguan = $time->now()->subWeek()->addDays(1); //tanggal 1 minggu lalu
-        $pemasukanMingguans = tbl_pemasukan::whereDate('created_at', '>=', $mingguan)->get();
+        $pemasukanMingguans = tbl_pemasukan::whereDate('created_at', '>=', $mingguan)->where('status', '1')->get();
         $pemasukanMingguanTotal = $pemasukanMingguans->sum('jml_masuk');
 
         //pemasukan 1 bulan ini
         $bulanan = $time->now()->startOfMonth(); //Bulan ini
-        $pemasukanBulanan = tbl_pemasukan::whereDate('created_at', '>=', $bulanan)->get();
+        $pemasukanBulanan = tbl_pemasukan::whereDate('created_at', '>=', $bulanan)->where('status', '1')->get();
         $pemasukanBulananTotal = $pemasukanBulanan->sum('jml_masuk');
 
         //pemasukan Harian
@@ -42,13 +42,13 @@ class DirekturController extends Controller
             $tanggalHarians[$i] = $mingguan->copy()->addDays($i)->format('d-M');
 
             // Query untuk mendapatkan data pemasukan harian
-            $pemasukanHarian = tbl_pemasukan::where('created_at', '=', $tanggalHarian)->get();
+            $pemasukanHarian = tbl_pemasukan::where('created_at', '=', $tanggalHarian)->where('status', '1')->get();
 
             // Menyimpan data harian ke dalam array
             $pemasukanHarians[$i] = $pemasukanHarian->sum('jml_masuk');
         }
 
-        $totalPemasukan = tbl_pemasukan::all()->sum('jml_masuk');
+        $totalPemasukan = tbl_pemasukan::all()->where('status', '1')->sum('jml_masuk');
 
         /* ------------------------------------- PEMASUKAN DAN PENGELUARAN TIAP 6 BULAN TERAKHIR ------------------------------------- */
         // data pemasukan dan pengeluaran sampai 6 bulan sebelumnya
@@ -64,17 +64,17 @@ class DirekturController extends Controller
             $tanggalBulanans[$i] = strtoupper($tanggalAwal->format('M'));
 
             // Ambil data untuk setiap bulan
-            $pemasukanBulanans[$i] = tbl_pemasukan::whereBetween('created_at', [$tanggalAwal, $tanggalAkhir])->get()->sum('jml_masuk');
-            $pengeluaranBulanans[$i] = tbl_pengeluaran::whereBetween('created_at', [$tanggalAwal, $tanggalAkhir])->get()->sum('jml_keluar');
+            $pemasukanBulanans[$i] = tbl_pemasukan::whereBetween('created_at', [$tanggalAwal, $tanggalAkhir])->where('status', '1')->get()->sum('jml_masuk');
+            $pengeluaranBulanans[$i] = tbl_pengeluaran::whereBetween('created_at', [$tanggalAwal, $tanggalAkhir])->where('status', '1')->get()->sum('jml_keluar');
         }
 
         /* ------------------------------------- PENGELUARAN MINGGUAN ------------------------------------- */
         //pengeluaran 1 minggu terakhir
-        $pengeluaranMingguans = tbl_pengeluaran::whereDate('created_at', '>=', $mingguan)->get();
+        $pengeluaranMingguans = tbl_pengeluaran::whereDate('created_at', '>=', $mingguan)->where('status', '1')->get();
         $pengeluaranMingguanTotal = $pengeluaranMingguans->sum('jml_keluar');
 
         //pengeluaran 1 bulan terakhir
-        $pengeluaranBulanan = tbl_pengeluaran::whereDate('created_at', '>=', $bulanan)->get();
+        $pengeluaranBulanan = tbl_pengeluaran::whereDate('created_at', '>=', $bulanan)->where('status', '1')->get();
         $pengeluaranBulananTotal = $pengeluaranBulanan->sum('jml_keluar');
 
         $pengeluaranHarians = [];
@@ -83,13 +83,13 @@ class DirekturController extends Controller
             $tanggalHarian = $mingguan->copy()->addDays($i)->format('Y-m-d');
 
             // Query untuk mendapatkan data pengeluaran harian
-            $pengeluaranHarian = tbl_pengeluaran::where('created_at', '=', $tanggalHarian)->get();
+            $pengeluaranHarian = tbl_pengeluaran::where('created_at', '=', $tanggalHarian)->where('status', '1')->get();
 
             // Menyimpan data harian ke dalam array
             $pengeluaranHarians[$i] = $pengeluaranHarian->sum('jml_keluar');
         }
 
-        $totalPengeluaran = tbl_pengeluaran::all()->sum('jml_keluar');
+        $totalPengeluaran = tbl_pengeluaran::all()->where('status', '1')->sum('jml_keluar');
 
         /* ------------------------------------- PERBANDINGAN PEMASUKAN DAN PENGELUARAN MINGGUAN ------------------------------------- */
         if ($totalPemasukan == 0 && $totalPengeluaran == 0) {
@@ -148,12 +148,20 @@ class DirekturController extends Controller
 
     public function cashflow()
     {
-        $divisis = tbl_divisi::all();
+        $divisis =  tbl_divisi::withCount('users')->with([
+            'pemasukans' => function ($query) {
+                $query->selectRaw('id_divisi, SUM(jml_masuk) as total_pemasukan')->where('status', '1')->groupBy('id_divisi');
+            },
+            'pengeluarans' => function ($query) {
+                $query->selectRaw('id_divisi, SUM(jml_keluar) as total_pengeluaran')->where('status', '1')
+                    ->groupBy('id_divisi');
+            },
+        ])->get();
 
-        $pemasukans = tbl_pemasukan::select('id_pemasukan as id', 'id_kategori', 'id_user', 'id_user_create', 'id_user_edit', 'jml_masuk as jumlah', 'tgl_pemasukan as tanggal', 'catatan', 'bukti_pemasukan as bukti', 'status', 'created_at')
+        $pemasukans = tbl_pemasukan::select('id_pemasukan as id', 'id_kategori', 'id_user', 'id_user_create', 'id_user_edit', 'jml_masuk as jumlah', 'tgl_pemasukan as tanggal', 'catatan', 'bukti_pemasukan as bukti', 'status', 'created_at')->where('status', '1')
             ->addSelect(DB::raw("'pemasukan' as jenis_transaksi"));
 
-        $pengeluarans = tbl_pengeluaran::select('id_pengeluaran as id', 'id_kategori', 'id_user', 'id_user_create', 'id_user_edit', 'jml_keluar as jumlah', 'tgl_pengeluaran as tanggal', 'catatan', 'bukti_pengeluaran as bukti', 'status', 'created_at')
+        $pengeluarans = tbl_pengeluaran::select('id_pengeluaran as id', 'id_kategori', 'id_user', 'id_user_create', 'id_user_edit', 'jml_keluar as jumlah', 'tgl_pengeluaran as tanggal', 'catatan', 'bukti_pengeluaran as bukti', 'status', 'created_at')->where('status', '1')
             ->addSelect(DB::raw("'pengeluaran' as jenis_transaksi"));
 
         $riwayatPemasukanPengeluaran = $pemasukans->union($pengeluarans)->orderBy('created_at', 'desc')->get();
--- app/tbl_divisi.php
+++ app/tbl_divisi.php
@@ -12,5 +12,20 @@ class tbl_divisi extends Model
     protected $fillable = [
         "nama_divisi",
     ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'id_divisi', 'id_divisi');
+    }
+
+    public function pemasukans()
+    {
+        return $this->hasManyThrough(tbl_pemasukan::class, User::class, 'id_divisi', 'id_user', 'id_divisi', 'id');
+    }
+
+    public function pengeluarans()
+    {
+        return $this->hasManyThrough(tbl_pengeluaran::class, User::class, 'id_divisi', 'id_user', 'id_divisi', 'id');
+    }    
 }
 
