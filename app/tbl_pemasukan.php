3583d65162d3bb7c5d43a7ce8737e3ff62621e46
Syukran <msyukran444@gmail.com>
2023-12-11 11:48:56 +0700

    Update relasi tbl pemasukan/pengeluaran

--- app/tbl_pemasukan.php
+++ app/tbl_pemasukan.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class tbl_pemasukan extends Model
 {
-    protected $table ="tbl_pemasukans";
-    protected $fillable =[
+    protected $table = "tbl_pemasukans";
+    protected $fillable = [
         "id_kategori",
         "id_user",
         "id_user_create",
@@ -15,4 +15,14 @@ class tbl_pemasukan extends Model
         "tgl_pemasukan",
         "status",
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function kategori()
+    {
+        return $this->belongsTo(tbl_kategori::class, 'id_kategori', 'id_kategori');
+    }
 }
--- app/tbl_pengeluaran.php
+++ app/tbl_pengeluaran.php
@@ -15,4 +15,14 @@ class tbl_pengeluaran extends Model
         "tgl_pengeluaran",
         "status",
     ];
+
+    public function user()
+    {        
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function kategori()
+    {
+        return $this->belongsTo(tbl_kategori::class, 'id_kategori', 'id_kategori');
+    }
 }
