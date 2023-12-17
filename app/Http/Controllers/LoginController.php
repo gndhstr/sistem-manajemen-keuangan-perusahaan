101a8fbff5b28bfea5ec63b3eb75df680883897c
nellaadrs <111825727+nellaadrs@users.noreply.github.com>
2023-12-17 21:12:22 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use App\user;
 
 class LoginController extends Controller
 {
@@ -12,38 +14,39 @@ class LoginController extends Controller
     return view("login/index");
   } 
   
-  public function authenticate(Request $request){
-    $crudentials = $request->only("username", "password");
-    if(Auth::attempt($crudentials)){
-        $user = Auth::user();
-        // dd($user);
+  public function authenticate(Request $request)
+    {
+        $credentials = $request->only("username", "password");
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
 
-        // role direktur
-        if($user->role == "1"){
-            return redirect()->intended("/admin");
+            // Mengambil data pengguna dari tabel users berdasarkan id
+            $userData = user::find($user->id);
+
+            if ($userData) {
+                // Menyimpan data pengguna dalam session
+                Session::put('userData', $userData);
+
+                // Logika redirect sesuai dengan peran pengguna
+                if ($user->role == "1") {
+                    return redirect()->intended("/admin");
+                } elseif ($user->role == "2") {
+                    return redirect()->intended("/direktur");
+                } elseif ($user->role == "3") {
+                    return redirect()->intended("/manajer");
+                } elseif ($user->role == "4") {
+                    return redirect()->intended("/karyawan");
+                }
+            }
         }
-        // role admin
-        elseif($user->role == "2"){
-            return redirect()->intended("/direktur");
-        }
-        // role manager
-        elseif($user->role == "3"){
-            return redirect()->intended("/manajer");
-        }
-        // role karyawan
-        elseif($user->role == "4"){
-            return redirect()->intended("/karyawan");
-        }
+
+        // Pemberitahuan kesalahan jika otentikasi gagal
+        return redirect(route('index'))->withErrors('Email atau password salah.');
     }
-     // Pemberitahuan kesalahan jika otentikasi gagal
-     return redirect(route('index'))->withErrors('Email atau password salah.');
-
-  }
   public function logout()
   {
    Auth::logout();
    return redirect(route('index'));
   }
-
-
 }
+
