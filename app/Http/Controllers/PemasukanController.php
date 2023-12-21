afb76e49db1e1913c764223c9bd811447083a623
khrisnaaaw <kina30052002@gmail.com>
2023-12-21 21:33:25 +0700

    pemasukan update, sma pengeluaran sudah aman semua

--- app/Http/Controllers/PemasukanController.php
+++ app/Http/Controllers/PemasukanController.php
@@ -9,6 +9,7 @@ use Dompdf\Options;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 
 class PemasukanController extends Controller
